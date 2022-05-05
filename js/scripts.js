78c103e83a14e8b49e3b5bac73c89e0c97c18069
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-05-04 19:22:12 -0700

    updates to UI and bussiness logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,67 +1,104 @@
-//Business Logic for Game
-
-function Game(dice, holdTurn, player1, player2) {
-  this.dice = dice;
+function Game(player1, player2) {
+  this.dice = 0;
   this.activeTurn = 0;
-  this.holdTurn = holdTurn;
-  this.player1 = 1;
-  this.player2 = 2;
-  this.currentPlayer = player1;
-}
-
-Game.prototype.turn = function (player) {
-  let check = player;
-  if (check === true) {
-    this.player1 = true;
-    this.player2 = false;
-    this.currentPlayer = true;
-    //this.player1Turn.roll();
-    console.log(this.currentPlayer);
-  }
-  else if (check === false) {
-    this.player1 = false;
-    this.player2 = true;
-    this.currentPlayer = false;
-    //this.player2Turn.roll();
-    console.log(this.currentPlayer);
-  }
-  //this.player1.roll();
-  console.log(this.currentPlayer);
+  this.player1 = player1;
+  this.player2 = player2;
+  this.currentPlayer = 1;
 }
 
 Game.prototype.roll = function () {
   const randomRoll = Math.floor(Math.random() * 6) + 1; // get a random number between 1 - 6
-  console.log(randomRoll);
   if (randomRoll === 1) { //check if number is === 1
-    this.activeTurn = 0; //change turn score to 0 
-    console.log(this.activeTurn);
-    this.turn(Player.activePlayer); //switch players
-    this.hold(this.currentPlayer);
+    this.dice = randomRoll; //show what dice roll is. 
+    this.activeTurn = 0; //change turn score to 0  
+    this.hold();
   } else if (randomRoll !== 1) {
+    this.dice = randomRoll; //show what dice roll is.
     this.activeTurn += randomRoll; //add roll to active turn total
-    this.turn(Player.activePlayer);
-    console.log(this.activeTurn);  //don't switch players
   }
 }
 
 //Business Logic for Players
-function Player(playerName, totalPoints, turnScore) {
+function Player(playerName) {
   this.playerName = playerName;
   this.totalPoints = 0;
   this.turnScore = 0;
   this.activePlayer;
 }
-Game.prototype.hold = function () {
-  if (this.currentPlayer === 1) {
-    this.totalPoints += this.activeTurn;
-    console.log(this.totalPoints);
-    this.currentPlayer = this.player2;
+
+Game.prototype.hold = function (player1, player2) {
+  console.log(this.player1, this.player2);
+  if (this.player2.playerName === "computer") {
+    this.computerPlay();
+  } else if (this.currentPlayer === 1) {
+    this.player1.totalPoints += this.activeTurn; //add active turn points to total points
+    this.currentPlayer = 2; //switch current player
+    this.roll(); //player two roll
     console.log(this.currentPlayer);
   } else if (this.currentPlayer === 2) {
-    this.totalPoints += this.activeTurn;
-    console.log(this.totalPoints);
-    this.currentPlayer = this.player1;
+    this.player2.totalPoints += this.activeTurn; //add active turn points to total points
+    this.currentPlayer = 1; //switch current player
+    this.roll(); //player 1 roll
     console.log(this.currentPlayer);
   }
 }
 
+Game.prototype.computerPlay = function () {
+  for (let i = 0; i <= 2; i++) {
+    this.roll();
+    this.player2.totalPoints += this.activeTurn;
+  }
+}
+//UI
+
+$(document).ready(function () {
+  $("#2players").click(function () {
+    $(".newGame1").show();
+    $("#pick-players").hide();
+  });
+  $("#computer").click(function () {
+    $("#pick-players").hide();
+    $(".newGame2").show();
+  });
+  $("#new-game1").submit(function (event) {
+    event.preventDefault();
+    $(".newGame").hide();
+    let player1 = new Player($("input#player1").val());
+    let player2 = new Player($("input#player2").val());
+    let newGame = new Game(player1, player2);
+    console.log(newGame);
+    $(".playerOneName").text(player1.playerName);
+    $(".playerTwoName").text(player2.playerName);
+    $("#roll-dice").click(function () {
+      newGame.roll();
+      $(".currentRoll").text(newGame.dice);
+      $(".round-score").text(newGame.activeTurn);
+    })
+    $("#hold").click(function () {
+      newGame.hold();
+      $(".p1-totalScore").text(player1.totalPoints);
+      $(".p2-totalScore").text(player2.totalPoints);
+    })
+  })
+
+  $("#new-game2").submit(function (event) {
+    event.preventDefault();
+    $(".newGame2").hide();
+    let player1 = new Player($("input#player1c").val());
+    let player2 = new Player("computer");
+    let newGame = new Game(player1, player2);
+    console.log(newGame);
+    $(".playerOneName").text(player1.playerName);
+    $(".playerTwoName").text("Computer");
+    $("#roll-dice").click(function () {
+      newGame.roll();
+      $(".currentRoll").text(newGame.dice);
+      $(".round-score").text(newGame.activeTurn);
+    })
+    $("#hold").click(function () {
+      newGame.hold();
+      $(".p1-totalScore").text(player1.totalPoints);
+      $(".p2-totalScore").text(player2.totalPoints);
+    })
+  })
+})
